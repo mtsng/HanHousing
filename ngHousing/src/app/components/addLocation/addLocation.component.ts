d6021debd445315414415e174e73a6851a53bf71
Michael Tseng <michael-tseng@outlook.com>
2018-02-01 12:02:14 -0500

    Added geocaching API

--- ngHousing/src/app/components/addLocation/addLocation.component.ts
+++ ngHousing/src/app/components/addLocation/addLocation.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Key } from '../../API_KEY';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
 
 @Component({
-  selector: 'app-add',
+  selector: 'app-addLocation',
   templateUrl: './addLocation.component.html',
   styleUrls: ['./addLocation.component.css']
 })
 
 export class AddLocationComponent{
-  
+  public input: string;
+  private url: string = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+  private result: any;
+  private key: string = '&key=' + Key.api_key;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  public submitAddress(){
+    //TODO null check
+    let address = this.input.replace(/\s/g, '+');
+    this.getCoordinates(address).subscribe(
+      data => {
+        this.result = data;
+        console.log(data);
+        console.log(data.results[0].geometry.location);
+      });
+  }
+
+  public getCoordinates(address: string){
+    return this.http.get(this.url + address + this.key);
+  }
 }
--- ngHousing/src/app/routing.ts
+++ ngHousing/src/app/routing.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { AddLocationComponent } from './components/addLocation/addLocation.component';
 
 export const appRoutes: Routes = [
   {
@@ -10,5 +11,9 @@ export const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'addLocation',
+    component: AddLocationComponent
   }
 ]
