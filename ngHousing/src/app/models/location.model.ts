141ad65f56997f65e17c16a6807a7743601d3bac
Michael Tseng <michael-tseng@outlook.com>
2018-02-02 14:54:58 -0500

    Edit location model

--- ngHousing/src/app/models/location.model.ts
+++ ngHousing/src/app/models/location.model.ts
@@ -1,4 +1,10 @@
+import { Contact } from './contact.model';
+
 export class Location {
+  contact: Contact;
   lat: number;
   lng: number;
+  address: string;
+  label?: string;
+  draggable: boolean;
 }
